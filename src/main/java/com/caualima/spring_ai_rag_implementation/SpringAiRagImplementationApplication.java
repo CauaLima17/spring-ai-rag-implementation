89b5384f7bcac74c053ba4628681ac84bd65bef7
Cauã de Lima <caua-l1@outlook.com>
2026-04-19 19:24:20 -0300

    chore: primeiro commit da aplicação

---
+++ src/main/java/com/caualima/spring_ai_rag_implementation/SpringAiRagImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.caualima.spring_ai_rag_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiRagImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiRagImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caualima/spring_ai_rag_implementation/SpringAiRagImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caualima.spring_ai_rag_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
